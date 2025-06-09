fd44eaf5899dbaa88bf5f23e9675aeec594cce17
TC2UREPEIR01\TC <yt33aa@gmail.com>
2025-06-09 17:30:52 +0800

    feat menu component

---
+++ src/app/feature/layout/menu/menu.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MenuComponent } from './pages/menu.component';
+
+export const menuRoutes: Routes = [
+  {
+    path:'',
+    component:MenuComponent
+  }
+]
