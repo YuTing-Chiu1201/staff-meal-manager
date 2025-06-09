c937528134ab4e5ae47c4a87cf9665c4031a4f71
TC2UREPEIR01\TC <yt33aa@gmail.com>
2025-06-09 16:15:02 +0800

    feat employee component

---
+++ src/app/feature/employee-manager/employee-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-manager',
+  imports: [],
+  templateUrl: './employee-manager.component.html',
+  styleUrl: './employee-manager.component.scss'
+})
+export class EmployeeManagerComponent {
+
+}
