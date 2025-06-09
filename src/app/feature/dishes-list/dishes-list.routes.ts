fcf39eb9d6f6cb7c4c42697ae968bb8389c9cf5e
TC2UREPEIR01\TC <yt33aa@gmail.com>
2025-06-09 17:30:35 +0800

    feat dish-list component

---
+++ src/app/feature/dishes-list/dishes-list.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { DishesListComponent } from './pages/dishes-list.component';
+
+export const dishRoutes: Routes = [
+  {
+    path: 'dishes-list',
+    component:DishesListComponent
+  }
+]
---
+++ src/app/feature/dishes-list/pages/dishes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dishes-list',
+  imports: [],
+  templateUrl: './dishes-list.component.html',
+  styleUrl: './dishes-list.component.scss'
+})
+export class DishesListComponent {
+
+}
